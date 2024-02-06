d3798aa3e95a0f65a147c05f73fa1c34cf726543
Emrah KONDUR <emrahkondur@hotmail.com>
2024-02-06 15:22:02 +0300

    #8 Moved All Settings to RedisUISettings Model (#9)
    Co-authored-by: Emrah Kondur <emrah.kondur@invicti.com>

---
+++ RedisUI/Models/RedisUISettings.cs
@@ -0,0 +1,15 @@
+﻿namespace RedisUI
+{
+    public class RedisUISettings
+    {
+        /// <summary>
+        /// Gets or sets the connection string for the Redis server.
+        /// </summary>
+        public string ConnectionString { get; set; } = "localhost";
+
+        /// <summary>
+        /// Gets or sets the path for the Redis server.
+        /// </summary>
+        public string Path { get; set; } = "/redis";
+    }
+}
