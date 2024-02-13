d89cec6f2b9e1a16107106f139f966dbd0d66d15
Emrah KONDUR <emrahkondur@hotmail.com>
2024-02-14 00:07:10 +0300

    Moved Icons (#26)
    Co-authored-by: Emrah Kondur <emrah.kondur@invicti.com>

--- RedisUI/Pages/Layout.cs
+++ RedisUI/Pages/Layout.cs
@@ -1,6 +1,8 @@
-﻿namespace RedisUI.Pages
+﻿using RedisUI.Contents;
+
+namespace RedisUI.Pages
 {
-    public static class MainLayout
+    public static class Layout
     {
         public static string Build(string section, string dbSize, int db, RedisUISettings settings)
         {
@@ -78,13 +80,10 @@
             <a class=""navbar-brand"" href=""..{settings.Path}"">RedisUI</a>
             <div class=""collapse navbar-collapse"" id=""navbarSupportedContent"">
               <ul class=""navbar-nav me-auto mb-2 mb-lg-0"">
-<a class=""navbar-brand"" title=""Keys"">
-                <svg xmlns=""http://www.w3.org/2000/svg"" width=""20"" height=""20"" fill=""currentColor"" class=""bi bi-key"" viewBox=""0 0 16 16"">
-                  <path d=""M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5""/>
-                  <path d=""M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0""/>
-                </svg>      
-                {dbSize}
-            </a>
+                <a class=""navbar-brand"" title=""Keys"">
+                    {Icons.KeyLg}      
+                    {dbSize}
+                </a>
                 <li class=""nav-item dropdown"">
                   <a id=""dblink"" class=""nav-link dropdown-toggle"" href=""#"" role=""button"" data-bs-toggle=""dropdown"" aria-expanded=""false"">
                     DB ({db})
@@ -111,10 +110,7 @@
               </ul>
             </div>            
             <a class=""navbar-brand"" title=""Statistics"" href=""..{settings.Path}/statistics"">
-                <svg xmlns=""http://www.w3.org/2000/svg"" width=""28"" height=""28"" fill=""currentColor"" class=""bi bi-speedometer2"" viewBox=""0 0 16 16"">
-                  <path d=""M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4M3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707M2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10m9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5m.754-4.246a.39.39 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.39.39 0 0 0-.029-.518z""/>
-                  <path fill-rule=""evenodd"" d=""M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A8 8 0 0 1 0 10m8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3""/>
-                </svg>
+                {Icons.Statistic}
             </a>
         </div>
     </nav>
